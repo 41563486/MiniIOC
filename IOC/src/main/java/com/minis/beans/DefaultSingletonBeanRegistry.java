b43f2a1458a9847c7541d325c87fa33618b60c5c
彼方 <3532953645@qq.com>
2023-05-21 17:16:50 +0800

    注释修改

--- IOC/src/main/java/com/minis/beans/DefaultSingletonBeanRegistry.java
+++ IOC/src/main/java/com/minis/beans/DefaultSingletonBeanRegistry.java
@@ -9,9 +9,10 @@ public class DefaultSingletonBeanRegistry implements SingletonBeanRegistry {
     //容器中存放所有bean的名称的列表
     protected List<String> beanNames = new ArrayList<>();
     //实例中存放所有bean实例的map
+    //ConcurrentHashMap允许多个线程同时读写，锁的粒度较小。不会锁住整个线程，使用了锁分段技术。内部的值是设定表的大小而不会动态调整
     protected Map<String, Object> singletons = new ConcurrentHashMap<>(256);
 
-
+    //synchronized保护线程安全，阻塞其他访问的线程
     public void registerSingleton(String beanName, Object singletonObject) {
         synchronized (this.singletons) {
             this.singletons.put(beanName, singletonObject);
@@ -21,20 +22,23 @@ public class DefaultSingletonBeanRegistry implements SingletonBeanRegistry {
 
     }
 
-
+    //获取bean单例方法
     public Object getSingleton(String beanName) {
         return this.singletons.get(beanName);
     }
 
-
+    //判断是否有bean单例
     public boolean containsSingleton(String beanName) {
+
         return this.singletons.containsKey(beanName);
     }
 
+    //获取bean集合内部bean
     public String[] getSingletonNames() {
+        //将ArrayList转换成数组再强制转换成String数组
         return (String[]) this.beanNames.toArray();
     }
-
+    //删除bean单例，使用synchronize保证线程的安全性
     public void removeSingleton(String beanName) {
         synchronized (this.singletons) {
             this.beanNames.remove(beanName);
--- IOC/src/main/java/com/minis/beans/SimpleBeanFactory.java
+++ IOC/src/main/java/com/minis/beans/SimpleBeanFactory.java
@@ -8,14 +8,18 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class SimpleBeanFactory extends DefaultSingletonBeanRegistry implements BeanFactory {
 
-    private Map<String,BeanDefinition> beanDefinitions=new ConcurrentHashMap<>(256);
+    //实例中存放所有bean定义的实例的map
+    //ConcurrentHashMap允许多个线程同时读写，锁的粒度较小。不会锁住整个线程，使用了锁分段技术。内部的值是设定表的大小而不会动态调整
+    private Map<String, BeanDefinition> beanDefinitions = new ConcurrentHashMap<>(256);
 
+    //定义bean定义实例的集合
+    private List<String> beanDefinitionNames = new ArrayList<>();
 
-        private List<String> beanDefinitionNames=new ArrayList<>();
+    //无参构造器
     public SimpleBeanFactory() {
     }
 
-    //getBean,容器的核心方法
+    //getBean,容器的核心方法，从单例bean集合内取出单例bean
     public Object getBean(String beanName) throws BeanException {
         //先尝试拿出bean实例
         Object singleton = this.singletons.get(beanName);
@@ -29,8 +33,6 @@ public class SimpleBeanFactory extends DefaultSingletonBeanRegistry implements B
             try {
                 //获取bean的定义
                 singleton = Class.forName(beanDefinition.getClassName()).newInstance();
-
-
             } catch (ClassNotFoundException e) {
                 throw new RuntimeException(e);
             } catch (InstantiationException e) {
@@ -46,10 +48,10 @@ public class SimpleBeanFactory extends DefaultSingletonBeanRegistry implements B
         return singleton;
     }
 
-    public void registerBeanDefinition(String name,BeanDefinition beanDefinition) {
-        this.beanDefinitions.put(name,beanDefinition);
+    public void registerBeanDefinition(String name, BeanDefinition beanDefinition) {
+        this.beanDefinitions.put(name, beanDefinition);
         this.beanDefinitionNames.add(name);
-        if (!beanDefinition.isLazyInit()){
+        if (!beanDefinition.isLazyInit()) {
             try {
                 getBean(name);
             } catch (BeanException e) {
@@ -57,12 +59,14 @@ public class SimpleBeanFactory extends DefaultSingletonBeanRegistry implements B
             }
         }
     }
-    public void removeBeanDefinition(String name){
+
+    public void removeBeanDefinition(String name) {
         this.beanDefinitions.remove(name);
         this.beanDefinitionNames.remove(name);
         this.removeSingleton(name);
     }
-    public BeanDefinition getBeanDefinition(String name){
+
+    public BeanDefinition getBeanDefinition(String name) {
         return this.beanDefinitions.get(name);
     }
 
